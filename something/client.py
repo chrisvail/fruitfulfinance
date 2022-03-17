a86da1e5349774f0ba4cb394c4dedd2e38a00269
Chris Vail <christopher.n.vail@gmail.com>
2022-03-17 01:08:16 +0000

    Initial commit - starting to structure the project

---
+++ something/client.py
@@ -0,0 +1,17 @@
+
+
+class Client:
+
+    def __init__(self):
+        self.office_distance
+        self.unit_count
+        self.contract_length
+        self.time_to_renewal
+
+
+class InterestedClient:
+
+    def __init__(self) -> None:
+        self.office_distance
+        self.office_size
+        
