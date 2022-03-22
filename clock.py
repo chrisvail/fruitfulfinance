2bd9a54a97ac174de98b0cc065c0ea8cd027d5aa
Chris Vail <christopher.n.vail@gmail.com>
2022-03-22 12:16:59 +0000

    Added sim clock to sim

--- clock.py
+++ clock.py
@@ -1,15 +1,18 @@
-
+from logging import getLogger
 
 class SimClock:
 
     def __init__(self) -> None:
         self.subscribers = []
-        self.step = 0
-
+        self._step = 0
+        self.logger = getLogger(__name__)
+        self.logger.info("Starting step: 0")
     
     def subscribe(self, subscriber):
         self.subscribers.append(subscriber)
 
     def step(self, actions):
+        self._step += 1
+        self.logger.info(f"Starting step: {self._step}")
         for sub in self.subscribers:
-            sub.notify(actions)
+            sub.step(actions)
--- factory.py
+++ factory.py
@@ -10,12 +10,18 @@ from shared.transport import Transport
 from expense import Expense
 from revenue import Revenue
 from distributions import Distribution
+from clock import SimClock
 
 
 
 class Simulation:
 
-    def __init__(self, clock, config) -> None:
+    def __init__(self, sim_details, steps) -> None:
+        # Create clock for the simulation
+        self.sim_clock = SimClock()
+
+        self.steps = steps
+
         # Create queues for passing information
         build_q = Queue()
         install_q = Queue()
@@ -26,32 +32,32 @@ class Simulation:
         self.revenue = Revenue(build_q, maintenance_q)
 
         # Create transportation
-        transport = Transport(**config["transport"])
+        transport = Transport(**sim_details["transport"])
 
         # Create germination station
         germination_station = GerminationStation(
             expense=self.expense, 
-            **config["germinationStation"]
+            **sim_details["germinationStation"]
         )
 
 
         # Customer Acquisition System
         # Create simulation cac values
-        cac_low = Distribution(**config["cac"]["cac_low"]).get_single()
-        cac_med = Distribution(**config["cac"]["cac_med"]).get_single()
-        cac_high = Distribution(**config["cac"]["cac_high"]).get_single()
-        config["activeClients"]["cac"]["values"] = [cac_low, cac_med, cac_high]
+        cac_low = Distribution(**sim_details["cac"]["cac_low"]).get_single()
+        cac_med = Distribution(**sim_details["cac"]["cac_med"]).get_single()
+        cac_high = Distribution(**sim_details["cac"]["cac_high"]).get_single()
+        sim_details["activeClients"]["cac"]["values"] = [cac_low, cac_med, cac_high]
 
         active_clients = ActiveClients(
             revenue=self.revenue, 
             expense=self.expense, 
             plant_requester=germination_station,
-            **config["activeClients"]
+            **sim_details["activeClients"]
         )
 
         customer_acquisition = CustomerAcquisition(
             activeClients=active_clients,
-            **config["customerAcquisition"]
+            **sim_details["customerAcquisition"]
         )
 
         # Build system
@@ -59,7 +65,7 @@ class Simulation:
             expense=self.expense,
             build_q=build_q,
             install_q=install_q,
-            **config["build"]
+            **sim_details["build"]
         )
 
         install = InstallUnits(
@@ -67,7 +73,7 @@ class Simulation:
             install_q=install_q,
             expense=self.expense,
             plant_store=germination_station,
-            **config["install"]
+            **sim_details["install"]
         )
 
         # Maintenance
@@ -76,9 +82,27 @@ class Simulation:
             plant_store=germination_station,
             transport=transport,
             expense=self.expense,
-            **config["maintenance"]
+            **sim_details["maintenance"]
         )
 
+        # Add objects to the simulation clock
+        self.sim_clock.subscribe(active_clients)
+        self.sim_clock.subscribe(customer_acquisition)
+        self.sim_clock.subscribe(self.revenue)
+        self.sim_clock.subscribe(build)
+        self.sim_clock.subscribe(install)
+        self.sim_clock.subscribe(maintenance)
+        self.sim_clock.subscribe(germination_station)
+        self.sim_clock.subscribe(self.expense)
+
+    def run(self):
+        actions = {
+
+        }
+        for _ in range(self.steps):
+            self.sim_clock.step(actions)
+        
+        
 
 if __name__ == "__main__":
     import yaml
@@ -88,7 +112,9 @@ if __name__ == "__main__":
     dictionary = yaml.safe_load(stream)
     pprint(dictionary, depth=3)
 
-    sim = Simulation(None, dictionary["sim_details"])
+    sim = Simulation(dictionary["sim_details"], dictionary["steps"])
+    sim.run()
+
 
     print(f"Revenue Total: {sim.revenue.total}")
     print(f"Expense Total: {sim.expense.total}")
